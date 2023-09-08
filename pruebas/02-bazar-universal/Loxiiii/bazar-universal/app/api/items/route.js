4ae67be9ebaea8a374bb5db646288c3929e6b562
MECALUX\fcampos <fjavier.campos@interlakemecalux.com>
2023-09-08 13:59:56 -0500

    /api/items?search route returns an array of results

--- pruebas/02-bazar-universal/Loxiiii/bazar-universal/app/api/items/route.js
+++ pruebas/02-bazar-universal/Loxiiii/bazar-universal/app/api/items/route.js
@@ -2,12 +2,12 @@ import { NextResponse } from 'next/server';
 import products from '../../../products.json';
 
 export async function GET(request, context) {
-//  const test = 'iphone';
-//  const { q } = request.query;
+  let searched = request.nextUrl.searchParams.get('search')
+//  const test = 'iphone'
   //  const prods = products.products.filter((prod) => prod.title.toLowerCase().includes(request.params.q.toLowerCase()));
   //    typeof (prod.title) === 'string');
 
-  //  const filteredProducts = products.products.filter((prod) => prod.title.toLowerCase().includes(q.toLowerCase()));
-  console.log('These are the params: ', context);
-  return NextResponse.json(context);
+    const filteredProducts = products.products.filter((prod) => prod.title.toLowerCase().includes(searched.toLowerCase()));
+//  console.log('This is the request search params: ', request.nextUrl.searchParams.get('search'));
+  return NextResponse.json(filteredProducts);
 }
--- pruebas/02-bazar-universal/Loxiiii/bazar-universal/components/Input.js
+++ pruebas/02-bazar-universal/Loxiiii/bazar-universal/components/Input.js
@@ -17,7 +17,8 @@ export default function Input() {
         onChange={handleSearchChange}
       />
       {/* <button>Search</button> */}
-      <Link href={`/items?search=${search}`}>Search</Link>
+      <Link href={`/api/items?search=${search}`}>Search</Link>
+      {/*<Link href={`/api/items`}>Search</Link>*/}
     </form>
   );
 }
